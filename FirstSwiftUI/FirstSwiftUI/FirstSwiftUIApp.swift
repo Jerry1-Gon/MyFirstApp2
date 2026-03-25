061bc467c9f0258125f96c300dad57625b640bf5
Jerry1-Gon <Jerardo.Gonzalez@hotmail.com>
2026-03-25 12:49:56 -0500

    Add files via upload

---
+++ FirstSwiftUI/FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Gonzalez, Jerardo on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
